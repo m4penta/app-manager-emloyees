31324bd610868e49c397f8e6bf7751bf8d3d1165
m4penta <80562619+m4penta@users.noreply.github.com>
2024-02-16 11:41:06 +0700

    cap nhat giao dien chinh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { AdminComponent } from './component/admin/admin.component';
+import { DepartmentComponent } from './component/department/department.component';
+import { EmployeesComponent } from './component/employees/employees.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: LoginComponent},
+  {path:'home', component: HomeComponent},
+  {path:'admin', component: AdminComponent},
+  {path:'department', component: DepartmentComponent},
+  {path:'employees', component: EmployeesComponent}
+];
---
+++ src/app/component/employees/employees.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employees',
+  standalone:true,
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
